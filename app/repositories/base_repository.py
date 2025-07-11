c92f13d29e9bb03f3935175dadc96f3cd3f1251a
pedroezekiel <ezekieleniola36@gmail.com>
2025-07-11 17:01:45 +0100

    add BaseRepository for common database operations

---
+++ app/repositories/base_repository.py
@@ -0,0 +1,21 @@
+from app.db import db
+
+class BaseRepository:
+    def __init__(self, model):
+        self.model = model
+
+    def get_by_id(self, id):
+        return self.model.query.get(id)
+
+    def get_all(self):
+        return self.model.query.all()
+
+    def create(self, **kwargs):
+        instance = self.model(**kwargs)
+        db.session.add(instance)
+        db.session.commit()
+        return instance
+
+    def delete(self, instance):
+        db.session.delete(instance)
+        db.session.commit()
