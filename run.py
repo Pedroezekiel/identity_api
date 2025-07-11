a9f411bf7951ee6ab8c1b2603aba3323e7102295
pedroezekiel <ezekieleniola36@gmail.com>
2025-07-11 17:05:20 +0100

    refactor run.py to use application factory and add shell context for database and user repository

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from app import create_app
+from app.db import db
+from app.models import User
+from app.repositories.user_repository import UserRepository
 
-app = Flask(__name__)
+app = create_app()
 
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "User": User,
+        "user_repo": UserRepository()
+    }
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
