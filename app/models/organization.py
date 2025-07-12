a50f598d7fdd0c73cc2b575a7c8956fd680a08f5
pedroezekiel <ezekieleniola36@gmail.com>
2025-07-12 15:39:42 +0100

    add description and created_by fields to organization model

--- app/models/organization.py
+++ app/models/organization.py
@@ -6,7 +6,9 @@ class Organization(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_by = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     # Relationships
     user_org_roles = db.relationship("UserOrganizationRole", back_populates="organization")
