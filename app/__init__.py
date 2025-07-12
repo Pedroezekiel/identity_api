06accdd46f68b25ff22a1e5945494e6a0cf20ea1
pedroezekiel <ezekieleniola36@gmail.com>
2025-07-12 15:38:59 +0100

    add role and invitation routes to application with predefined roles seeding

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,11 @@ from config.config import Config
 from dotenv import load_dotenv
 from app.routes.auth_routes import auth_bp
 from app.routes.organization_routes import organization_bp
+from app.routes.role_routes import role_bp
+from app.routes.user_org_role_routes import user_org_role_bp
+from app.routes.invitation_routes import invitation_bp
+from app.services.role_service import RoleService
+from app.repositories.role_repository import RoleRepository
 import os
 
 def create_app():
@@ -21,7 +26,12 @@ def create_app():
     # Import models to register them
     with app.app_context():
         db.create_all()
+        # Seed predefined roles
+        RoleService(RoleRepository()).seed_predefined_roles()
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(organization_bp)
+    app.register_blueprint(role_bp)
+    app.register_blueprint(user_org_role_bp)
+    app.register_blueprint(invitation_bp)
     return app
